b8a7cb86a70101a8659e27f64bd0ae5bc75425ae
acarlafiori <ccarlafiori@gmail.com>
2023-04-22 09:30:52 -0300

    Semilla y parametros

--- src/lightgbm/z424_lightgbm_final.r
+++ src/lightgbm/z424_lightgbm_final.r
@@ -20,13 +20,13 @@ PARAM$input$dataset       <- "./datasets/dataset_pequeno.csv"
 PARAM$input$training      <- c( 202107 )   #meses donde se entrena el modelo
 PARAM$input$future        <- c( 202109 )   #meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla           <- 102191
+PARAM$finalmodel$semilla           <- 661201
 
-PARAM$finalmodel$num_iterations    <-  559
-PARAM$finalmodel$learning_rate     <-    0.0100746999
-PARAM$finalmodel$feature_fraction  <-    0.5144127527
-PARAM$finalmodel$min_data_in_leaf  <-    505
-PARAM$finalmodel$num_leaves        <-     44
+PARAM$finalmodel$num_iterations    <-  448
+PARAM$finalmodel$learning_rate     <-    1.00446E+14  
+PARAM$finalmodel$feature_fraction  <-    7.49323E+14
+PARAM$finalmodel$min_data_in_leaf  <-    309
+PARAM$finalmodel$num_leaves        <-     273
 
 
 PARAM$finalmodel$max_bin           <-     31
