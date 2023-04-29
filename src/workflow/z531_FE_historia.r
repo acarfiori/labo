ffe27bb925796fbf33ab01cffa87e4c9085aa788
acarlafiori <ccarlafiori@gmail.com>
2023-04-29 14:11:11 -0300

    Cambiar parametros

--- src/workflow/z531_FE_historia.r
+++ src/workflow/z531_FE_historia.r
@@ -25,16 +25,16 @@ PARAM$experimento <- "FE5310"
 
 PARAM$exp_input  <- "DR5210"
 
-PARAM$lag1  <- TRUE
-PARAM$lag2  <- FALSE
+PARAM$lag1  <- FALSE
+PARAM$lag2  <- TRUE
 PARAM$lag3  <- FALSE
 
 PARAM$Tendencias1$run  <- TRUE
 PARAM$Tendencias1$ventana  <- 6
-PARAM$Tendencias1$tendencia  <- TRUE
+PARAM$Tendencias1$tendencia  <- FALSE
 PARAM$Tendencias1$minimo  <- FALSE
 PARAM$Tendencias1$maximo  <- FALSE
-PARAM$Tendencias1$promedio  <- FALSE
+PARAM$Tendencias1$promedio  <- TRUE
 PARAM$Tendencias1$ratioavg  <- FALSE
 PARAM$Tendencias1$ratiomax  <- FALSE
 
@@ -48,13 +48,13 @@ PARAM$Tendencias2$ratioavg  <- FALSE
 PARAM$Tendencias2$ratiomax  <- FALSE
 
 
-PARAM$RandomForest$run  <- FALSE
+PARAM$RandomForest$run  <- TRUE
 PARAM$RandomForest$num.trees  <- 40
-PARAM$RandomForest$max.depth  <-  5
+PARAM$RandomForest$max.depth  <-  4
 PARAM$RandomForest$min.node.size  <- 500
 PARAM$RandomForest$mtry  <- 15
 
-PARAM$CanaritosAsesinos  <- 0
+PARAM$CanaritosAsesinos  <- 0.8
 # FIN Parametros del script
 
 #------------------------------------------------------------------------------
